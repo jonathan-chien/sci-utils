667fff487315a63b799c4a3591cea87ebf06caad
Jonathan Chien <jonathanmchien@gmail.com>
2025-08-15 14:53:12 -0500

    Modify some dotted paths; successful test of config summarizing system for all configs except reproducibility, which currently will not be summarized

--- src/sci_utils/records.py
+++ src/sci_utils/records.py
@@ -9,64 +9,17 @@ from general_utils import serialization as serialization_utils
 from general_utils import configops as configops_utils
 
 
-# Define registry of extractor functions.
-REGISTRY = {
-    'dataset': {
-        'hypercube_dim': 'sequences_cfg.elem.args_cfg.num_dims',
-        'pos_vertices': 'sequences_cfg.elem.args_cfg.inclusion_set',
-        'pos_vertices_pmf': 'sequences_cfg.elem.args_cfg.vertices_pmfs.0',
-        'neg_vertices_pmf': 'sequences_cfg.elem.args_cfg.vertices_pmfs.1',
-        'pos_seq_lengths': 'sequences_cfg.seq_lengths.lengths.pos.support',
-        'pos_seq_lengths_pmf': 'sequences_cfg.seq_lengths.lengths.pos.pmf',
-        'neg_seq_lengths': 'sequences_cfg.seq_lengths.lengths.neg.support',
-        'neg_seq_lengths_pmf': 'sequences_cfg.seq_lengths.lengths.neg.pmf',
-        'train_size': 'split_cfg.train',
-        'val_size': 'split_cfg.val',
-        'test_size': 'split_cfg.test'
-    },
-    'model': {
-        'input_network': 'input_network.path',
-        'input_network_layer_sizes': 'input_network.args_cfg.layer_sizes',
-        'input_network_nonlinearities': 'input_network.args_cfg.nonlinearities',
-        'input_network_dropouts' : 'input_network.args_cfg.dropouts',
-        'rnn_type': 'rnn.path',
-        'rnn_input_size': 'rnn.args_cfg.input_size',
-        'rnn_hidden_size': 'rnn.args_cfg.hidden_size',
-        'rnn_nonlinearity': (
-            lambda model_cfg: configops_utils.traverse_dotted_path(model_cfg, 'rnn.args_cfg.nonlinearity')
-            if configops_utils.traverse_dotted_path(model_cfg, 'rnn.path').endswith('RNN') 
-            else None
-        ),
-        'readout_network': 'readout_network.path',
-        'readout_network_layer_sizes': 'readout_network.args_cfg.layer_sizes',
-        'readout_network_nonlinearities': 'readout_network.args_cfg.nonlinearities',
-        'readout_network_dropouts': 'readout_network.args_cfg.dropouts',
-    },
-    'training': {
-        'loss_terms': 'train_fn_cfg.loss_terms.*.name',
-        'loss_weights': 'train_fn_cfg.loss_terms.*.weight',
-        'optimizers': 'train_fn_cfg.loss_terms.*.optimizer.path',
-        'learning_rates': 'train_fn_cfg.loss_terms.*.optimizer.args_cfg.lr',
-        'train_batch_size': 'train_fn_cfg.dataloader.args_cfg.batch_size',
-        'val_batch_size': 'train_fn_cfg.evaluation.dataloader.args_cfg.batch_size',
-        'early_stopping_strategy': 'train_fn_cfg.early_stopping.args_cfg.strategy.path',
-        'early_stopping_patience': 'train_fn_cfg.early_stopping.args_cfg.strategy.args_cfg.patience',
-        'early_stopping_tol': 'train_fn_cfg.early_stopping.args_cfg.strategy.args_cfg.tol',
-        'num_epochs': 'train_fn_cfg.num_epochs',
-    },
-    'testing': {
-        'loss_terms': 'eval_fn_cfg.loss_terms.*.name',
-        'loss_weights': 'eval_fn_cfg.loss_terms.*.weight',
-        'batch_size': 'eval_fn_cfg.dataloader.args_cfg.batch_size',
-    }
-}
+@dataclass(slots=True)
+class ConfigSummaryRow:
+    """Dataclass for one row of csv file."""
+    timestamp: str
+    config_kind: str
+    config_id: str
+    note: str
 
 def summarize_cfg(cfg, dotted_path_registry: Dict[str, Union[str, Callable]]) -> Dict[str, Any]:
-    # return {
-    #     key : configops_utils.traverse_dotted_path(cfg, val) if isinstance(val, str)
-    #             else val(cfg) if callable(val) else val
-    #     for key, val in dotted_path_registry.items()
-    # }
+    """ 
+    """
     summary = {}
     for key, val in dotted_path_registry.items():
         if isinstance(val, str):
@@ -80,92 +33,17 @@ def summarize_cfg(cfg, dotted_path_registry: Dict[str, Union[str, Callable]]) ->
             )
     return summary
 
-
-
-# # Decorator to register extractor functions.
-# def register_extractor(config_kind):
-#     """Decorator factory."""
-#     def register_kind(fn):
-#         """Register extractor function under provided key of _EXTRACTORS dict."""
-#         _EXTRACTORS[config_kind] = fn
-#         return fn
-#     return register_kind
-
-# # -------------------------- Extractor functions ----------------------------- #
-# # Alias getter function.
-# def _get(cfg, dotted_path: str) -> Any:
-#     """Get value from config using dotted path."""
-#     if not is_dataclass(cfg):
-#         raise TypeError(
-#             f"Expected config to be a dataclass, but got {type(cfg)}."
-#         )
-#     return configops_utils.get(cfg, dotted_path)
-
-# @register_extractor('dataset')
-# def extract_dataset_summary(data_cfg) -> Dict[str, Any]:
-#     return {
-#         'hypercube_dim': _get(data_cfg, 'sequences_cfg.elem.args_cfg.num_dims'),
-#         'pos_vertices': _get(data_cfg, 'sequences_cfg.elem.args_cfg.inclusion_set'),
-#         'pos_vertices_pmf': _get(data_cfg, 'sequences_cfg.elem.args_cfg.vertices_pmfs.0'),
-#         'neg_vertices_pmf': _get(data_cfg, 'sequences_cfg.elem.args_cfg.vertices_pmfs.1'),
-#         'pos_seq_lengths': _get(data_cfg, 'sequences_cfg.seq_lengths.lengths.pos.support'),
-#         'pos_seq_lengths_pmf': _get(data_cfg, 'sequences_cfg.seq_lengths.lengths.pos.pmf'),
-#         'neg_seq_lengths': _get(data_cfg, 'sequences_cfg.seq_lengths.lengths.neg.support'),
-#         'neg_seq_lengths_pmf': _get(data_cfg, 'sequences_cfg.seq_lengths.lengths.neg.pmf'),
-#         'train_size': _get(data_cfg, 'data_cfg.split_cfg.train'),
-#         'val_size': _get(data_cfg, 'data_cfg.split_cfg.val'),
-#         'test_size': _get(data_cfg, 'data_cfg.split_cfg.test')
-#     }
-
-# @register_extractor('model')
-# def extract_model_summary(model_cfg) -> Dict[str, Any]:
-#     return {
-#         'input_network' : _get(model_cfg, 'input_network.path'),
-#         'input_network_layer_sizes' : _get(model_cfg, 'input_network.args_cfg.layer_sizes'),
-#         'input_network_nonlinearities' : _get(model_cfg, 'input_network.args_cfg.nonlinearities'),
-#         'input_network_dropouts' : _get(model_cfg, 'input_network.args_cfg.dropouts'),
-#         'rnn_type' : _get(model_cfg, 'rnn.path'),
-#         'rnn_input_size' : _get(model_cfg, 'rnn.args_cfg.input_size'),
-#         'rnn_hidden_size' : _get(model_cfg, 'rnn.args_cfg.hidden_size'),
-#         'rnn_nonlinearity' : _get(model_cfg, 'rnn.args_cfg.nonlinearity') if _get(model_cfg, 'rnn.path').endswith('RNN') else None,
-#         'readout_network' : _get(model_cfg, 'readout_network.path'),
-#         'readout_network_layer_sizes' : _get(model_cfg, 'readout_network.args_cfg.layer_sizes'),
-#         'readout_network_nonlinearities' : _get(model_cfg, 'readout_network.args_cfg.nonlinearities'),
-#         'readout_network_dropouts' : _get(model_cfg, 'readout_network.args_cfg.dropouts'),
-#     }
-
-# @register_extractor('training')
-# def extract_training_summary(training_cfg) -> Dict[str, Any]:
-#     return {
-#         'loss_terms': _get(training_cfg, 'train_fn_cfg.loss_terms.*.name'),
-#         'loss_weights': 'train_fn_cfg.loss_terms.*.weight',
-#         'optimizers': 'train_fn_cfg.loss_terms.*.optimizer.path',
-#         'learning_rates': 'train_fn_cfg.loss_terms.*.optimizer.args_cfg.lr',
-#         'train_batch_size': 'train_fn_cfg.dataloader.args_cfg.batch_size',
-#         'val_batch_size': 'train_fn_cfg.evaluation.dataloader.args_cfg.batch_size',
-#         'early_stopping_strategy': 'train_fn_cfg.early_stopping.args_cfg.strategy.path',
-#         'early_stopping_patience': 'train_fn_cfg.early_stopping.args_cfg.strategy.args_cfg.patience',
-#         'early_stopping_tol': 'train_fn_cfg.early_stopping.args_cfg.strategy.args_cfg.tol',
-#         'num_epochs': 'train_fn_cfg.num_epochs',
-#     }
-
-
-# Dataclass for one row of csv file.
-@dataclass(slots=True)
-class ConfigSummaryRow:
-    timestamp: str
-    config_kind: str
-    config_id: str
-    note: str
-
 def summarize_cfg_to_xlsx(
     cfg_filepath,
     *,
     config_kind: str,
     config_id: str,
     note: str,
+    dotted_path_registry: Dict[str, Union[str, Callable]],
     xlsx_filepath: Union[str, Path] = Path('configs/logs.xlsx')
 ):
+    """ 
+    """
     cfg = serialization_utils.deserialize(cfg_filepath)
     xlsx_filepath = Path(xlsx_filepath)
 
@@ -179,18 +57,11 @@ def summarize_cfg_to_xlsx(
         )
     )
 
-    # extractor = _EXTRACTORS.get(config_kind)
-    # if extractor:
-    #     summary = extractor(cfg) if extractor else {}
-    # else:
-    #     raise ValueError(
-    #         f"Provided `config_kind` {config_kind} did not match any entries "
-    #         "in the _EXTRACTORS dictionaries. These are the registered extractors: "
-    #         f"{list(_EXTRACTORS.keys())}."
-    #     )
-    summary = summarize_cfg(cfg, REGISTRY[config_kind])
-
+    # Extract summary as dict and add to default items.
+    summary = summarize_cfg(cfg, dotted_path_registry)
     new_row = {**base_row, **summary}
+
+    # Append new row to xlsx file.
     _append_row_to_sheet_upgrading_schema(
         xlsx_filepath=xlsx_filepath, 
         sheet_name=config_kind, 
@@ -203,10 +74,12 @@ def _append_row_to_sheet_upgrading_schema(
     sheet_name: str,
     new_row: Dict
 ):
-    # Create directory housing 
+    """ 
+    """
+    # Create directory housing workbook if it doesn't exist.
     xlsx_filepath.parent.mkdir(parents=True, exist_ok=True)
 
-    # Check for existing sheet and create if it doesn't exist.
+    # Check existing workbook for existing sheet and create if it doesn't exist.
     if xlsx_filepath.exists():
         try:
             # Retrieve existing sheet.
